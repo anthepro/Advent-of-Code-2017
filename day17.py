d3f390aca4ff11b7285a709ce5c907fe7fa8e066
Ante Sikic <31769514+anthepro@users.noreply.github.com>
2017-12-17 15:33:08 +0100

    day17

--- day17.py
+++ day17.py
@@ -4,23 +4,22 @@ def part1(step):
     for x in range(2017):
         pos = (pos + step + 1) % (x + 1)
         spinlock.insert(pos, x + 1)
-    return print(spinlock[(spinlock.index(2017)+1)%(x+2)]), (spinlock.index(0)+1) % (x + 2)
+    return spinlock[spinlock.index(2017)+1]
 
 def part2(step):
     pos = 0
     res = 0
-    _, target = part1(step)
     x = 0
     while x < 50000000:
         skip = max(1, (x - pos) // step)
         x += skip
         pos = (pos + skip * (step + 1)) % (x + 1)
-        if pos == target:
+        if pos == 0:
             res = x + 1
     return res
 
 def main():
-    print(part2(367))
+    print(part1(367), part2(367))
 
 if __name__=='__main__':
     main()
